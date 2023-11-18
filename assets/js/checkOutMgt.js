b1f275f279b7fff49b4e07212e829da89bb3160b
Afsalkp7 <afsalkpmanu31@gmail.com>
2023-11-18 12:26:54 +0530

    checkout completed

--- assets/js/checkOutMgt.js
+++ assets/js/checkOutMgt.js
@@ -10,3 +10,58 @@ function fillAddressFields(firstName, lastName, email, phone, address, address2,
     document.getElementById('pin').value = pin;
     document.getElementById('id').value = id
 }
+
+document.querySelectorAll(".deleteAddress").forEach((btn) => {
+    btn.addEventListener("click", async (event) => {
+      const adrId = await event.target.getAttribute("data-id");
+  
+      try {
+        const response = await fetch(`/checkout/delete/${adrId}`);
+        if (response.ok) {
+          const addressData = await response.json();
+          const categoryElement = document.getElementById("addressDetails");
+          categoryElement.innerHTML = `
+                <h4>Confirm Delete this address</h4><br>
+                <form id="deleteData">
+                  <input type="hidden" name="adr_id" value="${addressData._id}">
+                  <button type="button" onclick="deleteAddress()" data-bs-toggle="modal" data-bs-dismiss="modal" class="btn btn-dark text-light mt-3">Confirm Delete</button>
+                </form>
+              `;
+  
+          // Show the modal
+          const Modal = new bootstrap.Modal(
+            document.getElementById("addressDeleteShow")
+          );
+          Modal.show();
+        } else {
+          console.error("Error fetching user data");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    });
+  });
+  
+  function deleteAddress() {
+    const form = document.getElementById("deleteData");
+    const formData = new FormData(form);
+    fetch("/checkout", {
+      method: "DELETE",
+      body: JSON.stringify(Object.fromEntries(formData)),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        window.location.reload()
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  
--- server/routes/user/checkoutRouter.js
+++ server/routes/user/checkoutRouter.js
@@ -1,38 +1,94 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../../model/productModel")
+const Product = require("../../model/productModel");
 const userCollection = require("../../model/userModel");
-const CartItem = require("../../model/cartModel")
+const CartItem = require("../../model/cartModel");
 const { authCart } = require("../../middlewere/user_auth");
 const Order = require("../../model/oraderModel");
 
-router.post("/",authCart,async(req,res)=>{
-    const userId = req.userId;
-    const orderAddress =  await Order.find({userId})
-    const cartItems = await CartItem.find({userId})
-    const cartProducts = [];
+router.post("/", authCart, async (req, res) => {
+  const userId = req.userId;
+  const orderAddress = await Order.find({ userId });
+  const cartItems = await CartItem.find({ userId });
+  const cartProducts = [];
 
-    for (let cartItem of cartItems){
-        const productId = cartItem.productId;
-        const quantity = cartItem.quantity;
+  for (let cartItem of cartItems) {
+    const productId = cartItem.productId;
+    const quantity = cartItem.quantity;
 
-        const cartContent = await Product.find({_id:productId})
-        if (cartContent){
-            cartProducts.push({cartContent,quantity})
-        }
+    const cartContent = await Product.find({ _id: productId });
+    if (cartContent) {
+      cartProducts.push({ cartContent, quantity });
     }
-    const grandTotal = req.body.grandToCheck
-    res.render("checkout",{grandTotal,cartProducts,orderAddress})
-})
+  }
+  const grandTotal = req.body.grandToCheck;
+  res.render("checkout", { grandTotal, cartProducts, orderAddress });
+});
 
-router.post("/order",authCart,async(req,res)=>{
-    const userId = req.userId;
-    const {firstName,lastName,email,phone,address,address2,country,state,pin} = req.body
+router.post("/order", authCart, async (req, res) => {
+  const userId = req.userId;
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    address,
+    address2,
+    country,
+    state,
+    pin,
+    id,
+  } = req.body;
+  if (id){
+    const existAdress = await Order.findOne({ userId, _id: id });
+    if (existAdress) {
+      const updateAddress = await Order.updateOne(
+        { userId },
+        { $set: req.body }
+      );
+      if (updateAddress) {
+        return res.render("orderSummery");
+      }
+    }
+  }
+   else {
     const checkOutData = new Order({
-        userId,firstName,lastName,email,phone,address,address2,country,state,pin
-    })
+      userId,
+      firstName,
+      lastName,
+      email,
+      phone,
+      address,
+      address2,
+      country,
+      state,
+      pin,
+    });
     await checkOutData.save();
-    res.render("payment")
+    res.render("orderSummery");
+  }
+});
+
+
+router.get("/delete/:id",async(req,res)=>{
+    addressId = req.params.id;
+    try {
+        const address = await Order.findOne({ _id: addressId });
+        
+        if (address) {
+          return res.json(address);
+        } else {
+          res.status(404).json({ error: 'address not found' });
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
 })
 
+router.delete("/",async(req,res)=>{
+    const addressId = req.body.adr_id;
+    const deleteAddress = await Order.findByIdAndRemove(addressId);
+    return res.json(deleteAddress)
+})
 module.exports = router;
