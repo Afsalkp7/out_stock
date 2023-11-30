f2d524f859625672fdbe0f3e8143f1e00aad866d
Afsalkp7 <afsalkpmanu31@gmail.com>
2023-11-30 09:47:54 +0530

    razorpay test completed...

--- server/routes/user/placeOrder.js
+++ server/routes/user/placeOrder.js
@@ -66,18 +66,36 @@ router.post("/", authCart, async (req, res) => {
     } catch (error) {
       res.render("error", { error });
     }
-  }else{
+  }
+});
+
+router.post("/create/orderId",(req,res)=>{
+  
     console.log(req.body);
     var options = {
-      amount: req.body.grandTotal, 
+      amount: req.body.amount, 
       currency: "INR",
       receipt: "rcp1"
     };
-    instance.orders.create(options, function(err, order) {
+    razorpay.orders.create(options, function(err, order) {
       console.log(order);
       res.send({orderId:order.id})
     });
-  }
-});
+  
+})
+
+router.post("/payment/verify",(req,res)=>{
+  let body = req.body.response.razorpay_order_id +"|"+req.body.response.razorpay_payment_id;
+  var crypto = require("crypto")
+  var expectedSignature = crypto.createHmac("sha256","G3Q4J9KXWuefR0bhohf8zKl1")
+              .update(body.toString())
+              .digest("hex")
+              console.log("sig received" ,req.body.response.razorpay_signature)
+              console.log("sig generated" ,expectedSignature);
+  var response = {"signatureIsValid":"false"}
+  if (expectedSignature === req.body.response.razorpay_signature)
+   response={"signatureIsValid":"true"}
+    res.send(response);
+})
 
 module.exports = router;
