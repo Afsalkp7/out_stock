ba858db5ccb696346c1d8b4cd326e5923f15769f
Afsalkp7 <138362420+Afsalkp7@users.noreply.github.com>
2024-01-02 02:57:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3900;
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
